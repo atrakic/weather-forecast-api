6d0155b4a016c15e7258b30c7761ff8a4f2b6b03
Admir Trakic <atrakic@users.noreply.github.com>
2023-01-03 17:34:49 +0100

    Improve env variables on deployments (#3)

--- tests/test.sh
+++ tests/test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
-service="weather-forecast-api"
+host="weather-forecast-api.local"
 url="localhost:80"
 
 counter=0
 while true; do
    counter=$((counter+1))
-   echo "$counter: $(curl -f -i -skX GET $url -H"Host: ${service}.local")"
+   echo "$counter: $(curl -fiskX GET $url -H"Host: ${host}")"
    echo
-   #curl -skX GET "$url/WeatherForecast" -H"Host: $service.local" | xargs # | jq ". | length"
+   curl -fisk GET "$url/WeatherForecast" -H"Host: ${host}" | xargs # | jq ". | length"
+   #curl -fisk GET "$url/metrics" -H"Host: ${host}"
    sleep 1
 done
